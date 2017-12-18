bf0417666bddfdb30d38e124ced5f9c5feaabae6
BrianAmin <tuf49787@temple.edu>
2017-12-18 11:06:08 -0500

    Code works, just need to add in AStar conditions

--- Project1Phase3/studentcode/dijkstra.m
+++ Project1Phase3/studentcode/dijkstra.m
@@ -22,8 +22,9 @@ end
 
 % The example code given here may be of use to you, but it is also possible
 % to solve the problem without using any of this
+num_expanded = 0;
 
-
+% Start and goal xy coordinates in world, convert to grid
 
 start = world2grid(map, start);
 goal = world2grid(map, goal);
@@ -40,7 +41,7 @@ shape = [rows cols];
 %Returns linear index from map given [row col] aka [i j]
 inds = reshape(1:rows*cols, rows, cols);
 
- % inds(i,j) will return the index ind of the cell with subscript [i j]
+% inds(i,j) will return the index ind of the cell with subscript [i j]
 % This will return the same value as sub2ind(n_ij, i, j)
 
 start_index = sub2ind(shape, start(1), start(2));
@@ -63,76 +64,198 @@ subs = [is(:) js(:)];
 % Set dummy values of the outputs so that the code will run
 
 %Algo starts here
-Q = 0; % unvisited set
+%Q = zeros(length(inds)); % unvisited set, 0 means unvisited; 1 means visited
 
+%cost = zeros(length(inds));
 end_node = inds(end);
 
+% Initialize cost
 for i=1:end_node
     cost(i) = inf;
-    
-    %parent(i) = [];
-    
-    Q = [Q i];
+    Q(i) = 0;
+    parent(i) = nan;
 end
 
-%Delte that first zero
-Q(1) = [];
-
+%Start node is visited
+%Q(start_index) = 1;
+ 
 % Start node cost is zero
-cost(1) = 0;
+cost(start_index) = 0;
 
+%parent = [];
 
-%goal_in_Q = 1;
-while ( ismember(goal_index, Q) )
-    lowest = inf;
-    % Find node with lowest cost within the costmap
-    for i=1:length(cost)
-        b = cost(i);
-        if (b < lowest)
-            lowest = b;
-            u = i;
+% While Q contains zeros
+while ( Q(goal_index) == 0 && any( cost(:) < inf) )
+    if astar == false
+        lowest = inf;
+        % Find node with lowest cost that is also unvisited
+        for i=1:length(cost)
+            b = cost(i);
+            if (b < lowest)
+                if (Q(i) == 0)
+                    lowest = b;
+                    u = i;
+                end
+            end
         end
     end
+    %[value index] = min(cost);
     
-    Q(u) = []; % remove node from unvisisted
+    %disp(u);
     
-    for v=(u+1):length(Q)
-        %d = cost(i) + cost(u,i) %Crunch total cost to
-        space = subs(Q(v),:);
-        is_occupied = getOccupancy(map,[space(1) space(2)],'grid')
-        space_world = grid2world(map,[space(1) space(2)]);
-        
-        base = subs(Q(u),:);
-        base_world = grid2world(map,[base(1) base(2)]);
-        
-        %if (is_occupied == 0)
-            total_cost = cost(u) + abs(sqrt( ((space_world(1) - base_world(1))^2) + ((space_world(2)-space_world(2)^2))   ));
-        %else
-        %    total_cost = cost(u) + inf;
-        %end
-        
-        if total_cost < cost(v)
-            cost(v) = total_cost;
-            parent(v) = v;
-        end
-        %total_cost = cost(i) + [
-    end
 
+    Q(u) = 1; % mark node as visited
+    
+    % Now we need to find neighbors of u
+    [urow ucol] = ind2sub(shape,u);
+    
+    u_row_above = [];
+    u_row_below = [];
+    u_col_left = [];
+    u_col_right = [];
+    
+    n_rows = [];
+    n_cols = [];
+    
+    % Find neighboring rows
+    if (urow == shape(1))
+        u_row_above = urow - 1;
+    elseif (urow == 1)
+        u_row_below = urow + 1;
+    else
+        u_row_below = urow + 1;
+        u_row_above = urow - 1;
+    end
+    
+    % Find neighboring columns
+    if (ucol == shape(2))
+        u_col_left = ucol - 1;
+    elseif (ucol == 1)
+        u_col_right = ucol + 1;
+    else
+        u_col_left = ucol - 1;
+        u_col_right = ucol + 1;
+    end
+    
+    
+    % Put into all in one matrix
+    if ( isempty(u_row_above) == 0)
+        n_rows = [n_rows, u_row_above];
+    end
+    n_rows = [n_rows, urow];
+    if (isempty(u_row_below) == 0)
+        n_rows = [n_rows, u_row_below];
+    end
+    
+
+    
+    if ( isempty(u_col_left) == 0)
+        n_cols = [n_cols, u_col_left];
+    end
+    n_cols = [n_cols,ucol];
+    if (isempty(u_col_right) == 0)
+        n_cols = [n_cols, u_col_right];
+    end
+    
+    neighbors = [];
+    for i=1:length(n_cols)
+        for j=1:length(n_rows)
+            if (n_rows(j) == urow && n_cols(i) == ucol)
+                neighbors = neighbors;
+            else
+                neighbors = [neighbors, sub2ind(shape,n_rows(j),n_cols(i))];
+            end
+        end
+    end
+%     n = [];
+%     for b=1:length(neighbors)
+%         v = neighbors(b);
+%         if (Q(v) == 0)
+%             n(i) = neighbors(b);
+%         end
+%     end
+    
+    for v=1:length(neighbors)
+        
+        head = grid2world(map,[urow ucol]);
+        [vrow,vcol] = ind2sub(shape,neighbors(v));
+        base = grid2world(map,[vrow vcol]);
+        
+        
+        cost_between = abs( sqrt( (head(1) - (base(1)^2)) + (head(2)-(base(2)^2))   ));
+        
+        alt = cost(u) + cost_between;
+        
+        if alt < cost(neighbors(v))
+            cost(neighbors(v)) = alt;
+            parent(neighbors(v)) = u;
+            num_expanded = num_expanded + 1;
+        end
+        
+    end
 end
+
+
 
 path = [];
-for i=2:length(parent)
-    path = [path ;subs(parent(i),:)];
+f_start = 0;
+[row, col] = ind2sub(shape,goal_index);
+%path = horzcat(path,[row;col]);
+path = [path;row,col];
+par = goal_index;
+while f_start == 0
+    par = parent(par);
+    if par == start_index
+        f_start = 1;
+    end
+    [row, col] = ind2sub(shape,par);
+    %path = horzcat(path,[row;col]);
+    path = [path;row,col];
 end
 
-path = grid2world(map,path);
 
-    
-    
-%take p to path
+
+
+% Reconstruct path
+
+
+
 
 %path = [];
-num_expanded = 0;
+%for i=1:length(parent)
+%    if (isnan(parent(i)) == 0)
+%        [row, col] = ind2sub(shape,parent(i));
+%        path = [path; row,col];
+%    end
+%end
+
+
+
+
+    
+
+    
+path = grid2world(map,path);
+    
+    
+
+
+    
+    
+%     
+% path = [];
+% for i=2:length(parent)
+%     path = [path ;subs(parent(i),:)];
+% end
+% 
+% path = grid2world(map,path);
+% 
+%     
+%     
+% %take p to path
+% 
+% %path = [];
+
 
 % =================== Your code ends here ===================
 
